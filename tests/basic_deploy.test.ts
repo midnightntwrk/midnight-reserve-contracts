344529786576ffe9286e1a01d3eeca70040fa67e
microproofs <kwhitemsg@gmail.com>
2025-09-29 16:00:45 +0700

    chore: working test now

--- tests/basic_deploy.test.ts
+++ tests/basic_deploy.test.ts
@@ -29,9 +29,16 @@ describe("Basic Deploy", () => {
   // Contract instances
   const techAuthTwoStage =
     new Contracts.PermissionedTechAuthTwoStageUpgradeElse();
+  const techAuthForever = new Contracts.PermissionedTechAuthForeverElse();
+  const techAuthLogic = new Contracts.PermissionedTechAuthLogicElse();
   const councilTwoStage =
     new Contracts.PermissionedCouncilTwoStageUpgradeElse();
+  const councilForever = new Contracts.PermissionedCouncilForeverElse();
+  const councilLogic = new Contracts.PermissionedCouncilLogicElse();
   const reserveForever = new Contracts.ReserveReserveForeverElse();
+  const reserveTwoStage = new Contracts.ReserveReserveTwoStageUpgradeElse();
+  const reserveLogic = new Contracts.ReserveReserveLogicElse();
+  const govAuth = new Contracts.GovAuthMainGovAuthElse();
 
   // Default network config values from aiken.toml
   const config = {
@@ -121,9 +128,9 @@ describe("Basic Deploy", () => {
 
         // Create upgrade state datum for technical authority two-stage
         const techAuthUpgradeState: Contracts.UpgradeState = [
-          new Contracts.PermissionedTechAuthLogicElse().Script.hash(), // logic script hash
+          techAuthLogic.Script.hash(), // logic script hash
           "", // mitigation_logic (empty initially)
-          new Contracts.GovAuthMainGovAuthElse().Script.hash(), // auth script hash
+          govAuth.Script.hash(), // auth script hash
           "", // mitigation_auth (empty initially)
           0n, // round
         ];
@@ -139,11 +146,9 @@ describe("Basic Deploy", () => {
           }),
         ];
 
-        console.log("HERE");
-
         const foreverAddress = addressFromValidator(
           NetworkId.Testnet,
-          new Contracts.PermissionedTechAuthForeverElse().Script,
+          techAuthForever.Script,
         );
 
         await emulator.expectValidTransaction(
@@ -152,9 +157,7 @@ describe("Basic Deploy", () => {
             .newTransaction()
             .addInput(techAuthOneShotUtxo) // No redeemer for regular UTxO
             .addMint(
-              PolicyId(
-                new Contracts.PermissionedTechAuthForeverElse().Script.hash(),
-              ),
+              PolicyId(techAuthForever.Script.hash()),
               new Map([[AssetName(""), 1n]]),
               PlutusData.fromCore({
                 items: [
@@ -172,9 +175,7 @@ describe("Basic Deploy", () => {
               PlutusData.newInteger(0n),
             )
             .provideScript(techAuthTwoStage.Script)
-            .provideScript(
-              new Contracts.PermissionedTechAuthForeverElse().Script,
-            )
+            .provideScript(techAuthForever.Script)
             .addOutput(
               TransactionOutput.fromCore({
                 address: PaymentAddress(techAuthTwoStageAddress.toBech32()),
@@ -223,12 +224,7 @@ describe("Basic Deploy", () => {
                 value: {
                   coins: 2_000_000n,
                   assets: new Map([
-                    [
-                      AssetId(
-                        new Contracts.PermissionedTechAuthForeverElse().Script.hash(),
-                      ),
-                      1n,
-                    ],
+                    [AssetId(techAuthForever.Script.hash()), 1n],
                   ]),
                 },
                 datum: serialize(
@@ -239,8 +235,6 @@ describe("Basic Deploy", () => {
             ),
         );
 
-        console.log("THERE");
-
         emulator.addUtxo(councilOneShotUtxo);
 
         // Transaction 2: Mint Council tokens (two-stage)
@@ -250,19 +244,110 @@ describe("Basic Deploy", () => {
         );
 
         const councilUpgradeState: Contracts.UpgradeState = [
-          "5555555555555555555555555555555555555555555555555555555555", // logic script hash
+          councilLogic.Script.hash(), // logic script hash
           "", // mitigation_logic (empty initially)
-          "6666666666666666666666666666666666666666666666666666666666", // auth script hash
+          govAuth.Script.hash(), // auth script hash
           "", // mitigation_auth (empty initially)
           0n, // round
         ];
+
+        const councilForeverState: Contracts.Multisig = [
+          2n,
+          PlutusData.fromCore({
+            items: [
+              fromHex("8200581c" + addr.asBase()?.getPaymentCredential().hash),
+              fromHex("8200581c" + addr.asBase()?.getStakeCredential().hash),
+            ],
+          }),
+        ];
+
+        const councilForeverAddress = addressFromValidator(
+          NetworkId.Testnet,
+          councilForever.Script,
+        );
 
         await emulator.expectValidTransaction(
           blaze,
           blaze
             .newTransaction()
             .addInput(councilOneShotUtxo) // No redeemer for regular UTxO
-            .provideScript(councilTwoStage.Script),
+            .addMint(
+              PolicyId(councilForever.Script.hash()),
+              new Map([[AssetName(""), 1n]]),
+              PlutusData.fromCore({
+                items: [
+                  fromHex(addr.asBase()?.getPaymentCredential().hash!),
+                  fromHex(addr.asBase()?.getStakeCredential().hash!),
+                ],
+              }),
+            )
+            .addMint(
+              PolicyId(councilTwoStage.Script.hash()),
+              new Map([
+                [AssetName(toHex(new TextEncoder().encode("main"))), 1n],
+                [AssetName(toHex(new TextEncoder().encode("staging"))), 1n],
+              ]),
+              PlutusData.newInteger(0n),
+            )
+            .provideScript(councilTwoStage.Script)
+            .provideScript(councilForever.Script)
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(councilTwoStageAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [
+                      AssetId(
+                        councilTwoStage.Script.hash() +
+                          toHex(new TextEncoder().encode("main")),
+                      ),
+                      1n,
+                    ],
+                  ]),
+                },
+                datum: serialize(
+                  Contracts.UpgradeState,
+                  councilUpgradeState,
+                ).toCore(),
+              }),
+            )
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(councilTwoStageAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [
+                      AssetId(
+                        councilTwoStage.Script.hash() +
+                          toHex(new TextEncoder().encode("staging")),
+                      ),
+                      1n,
+                    ],
+                  ]),
+                },
+                datum: serialize(
+                  Contracts.UpgradeState,
+                  councilUpgradeState,
+                ).toCore(),
+              }),
+            )
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(councilForeverAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [AssetId(councilForever.Script.hash()), 1n],
+                  ]),
+                },
+                datum: serialize(
+                  Contracts.Multisig,
+                  councilForeverState,
+                ).toCore(),
+              }),
+            ),
         );
 
         // Transaction 3: Mint Reserve tokens (forever)
@@ -273,12 +358,93 @@ describe("Basic Deploy", () => {
           reserveForever.Script,
         );
 
+        const reserveTwoStageAddress = addressFromValidator(
+          NetworkId.Testnet,
+          reserveTwoStage.Script,
+        );
+
+        const reserveUpgradeState: Contracts.UpgradeState = [
+          reserveLogic.Script.hash(), // logic script hash
+          "", // mitigation_logic (empty initially)
+          govAuth.Script.hash(), // auth script hash
+          "", // mitigation_auth (empty initially)
+          0n, // round
+        ];
+
         await emulator.expectValidTransaction(
           blaze,
           blaze
             .newTransaction()
             .addInput(reserveOneShotUtxo) // No redeemer for regular UTxO
-            .provideScript(reserveForever.Script),
+            .addMint(
+              PolicyId(reserveForever.Script.hash()),
+              new Map([[AssetName(""), 1n]]),
+              PlutusData.newInteger(0n),
+            )
+            .addMint(
+              PolicyId(reserveTwoStage.Script.hash()),
+              new Map([
+                [AssetName(toHex(new TextEncoder().encode("main"))), 1n],
+                [AssetName(toHex(new TextEncoder().encode("staging"))), 1n],
+              ]),
+              PlutusData.newInteger(0n),
+            )
+            .provideScript(reserveForever.Script)
+            .provideScript(reserveTwoStage.Script)
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(reserveTwoStageAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [
+                      AssetId(
+                        reserveTwoStage.Script.hash() +
+                          toHex(new TextEncoder().encode("main")),
+                      ),
+                      1n,
+                    ],
+                  ]),
+                },
+                datum: serialize(
+                  Contracts.UpgradeState,
+                  reserveUpgradeState,
+                ).toCore(),
+              }),
+            )
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(reserveTwoStageAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [
+                      AssetId(
+                        reserveTwoStage.Script.hash() +
+                          toHex(new TextEncoder().encode("staging")),
+                      ),
+                      1n,
+                    ],
+                  ]),
+                },
+                datum: serialize(
+                  Contracts.UpgradeState,
+                  reserveUpgradeState,
+                ).toCore(),
+              }),
+            )
+            .addOutput(
+              TransactionOutput.fromCore({
+                address: PaymentAddress(reserveForeverAddress.toBech32()),
+                value: {
+                  coins: 2_000_000n,
+                  assets: new Map([
+                    [AssetId(reserveForever.Script.hash()), 1n],
+                  ]),
+                },
+                datum: PlutusData.fromCbor(HexBlob("01")).toCore(),
+              }),
+            ),
         );
       });
     });
